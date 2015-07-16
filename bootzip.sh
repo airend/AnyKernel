8cff6dcb9c8ee9494a282851bcae21dd4d395d24
D. Andrei Măceș <dmaces@nd.edu>
2015-07-16 15:46:10 -0400

    Adaptations for Nook HDs (no image unpack/repack necessary)

---
+++ bootzip.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd ~/Android/KernelPackr; OUT=../REPO/out
+
+if [ -n "$1" ]; then DEV=$1; else DEV=ovation; fi
+
+rm -rf boot.img system; mkdir -p system/lib/modules
+
+cp $OUT/target/product/$DEV/boot.img .
+
+cp $OUT/target/product/$DEV/system/lib/modules/* system/lib/modules
+
+if [ -f "../kernel-$DEV.zip" ]; then mv ../kernel-$DEV.zip ../kernel-$DEV.zip.bak; fi
+
+zip -r9 ../kernel-$DEV.zip boot.img system META-INF; rm -rf boot.img system
