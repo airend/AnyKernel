4f6becf4d4026cbc161ed4f24fbbbb791f70f1b6
D. Andrei Măceș <dmaces@nd.edu>
2015-07-16 19:48:55 -0400

    Include dynamic assert for "ro.product.device"

--- bootzip.sh
+++ bootzip.sh
@@ -4,6 +4,12 @@ cd ~/Android/AnyKernel; OUT=../REPO/out
 
 if [ -n "$1" ]; then DEV=$1; else DEV=ovation; fi
 
+ASSERT='assert(getprop("ro.product.device") == "variant" || getprop("ro.build.product") == "variant" || abort("This package is for device: variant; this device is " + getprop("ro.product.device") + "."););'
+
+echo $ASSERT | sed -e "s/variant/${DEV}/g" > META-INF/com/google/android/updater-script
+
+cat updater-script >> META-INF/com/google/android/updater-script
+
 rm -rf boot.img system; mkdir -p system/lib/modules
 
 cp $OUT/target/product/$DEV/boot.img .
