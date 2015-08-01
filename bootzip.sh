8b910990e3a527c77b7aa01f8c5b92a8aec9376d
D. Andrei Măceș <dmaces@nd.edu>
2015-08-01 14:41:59 -0400

    Add bootzip.sh helper script

---
+++ bootzip.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+KFLAV=Elefrant-N5-express
+
+cd ~/Android/KernelPackr; OUT=../REPO/kernel/lge/hammerhead
+
+cp -f $OUT/arch/arm/boot/zImage-dtb boot/elex.zImage
+
+if [ -f "../$KFLAV.zip" ]; then mv ../$KFLAV.zip ../$KFLAV.zip.bak; fi
+
+zip -r9 ../$KFLAV.zip META-INF boot config system thermal; rm -f boot/elex.zImage
