af4b86536593679e2cabda7d054e4cec37cc2ad4
D. Andrei Măceș <dmaces@nd.edu>
2016-01-24 22:30:56 -0500

    Add bootzip.sh helper script & ignore kernel images, modules, etc.

---
+++ bootzip.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+KFLAV=Franconaut-AK2
+
+cd ~/Android/KernelPackr; OUT=../REPO/kernel/lge/hammerhead
+
+cp -f $OUT/arch/arm/boot/zImage-dtb zImage
+
+if [ -f "../$KFLAV.zip" ]; then mv ../$KFLAV.zip ../$KFLAV.zip.bak; fi
+
+zip -r9 ../$KFLAV.zip anykernel.sh META-INF ramdisk tools zImage; rm -f zImage
