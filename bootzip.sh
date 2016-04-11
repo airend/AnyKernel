f75fed0bea8c2b32e86cec4ffa6a89c46937cc10
D. Andrei Măceș <dmaces@nd.edu>
2016-04-11 18:54:23 -0400

    bootzip.sh: Keep zImage around

--- bootzip.sh
+++ bootzip.sh
@@ -8,4 +8,4 @@ cp -f $OUT/arch/arm/boot/zImage-dtb zImage
 
 if [ -f "../$KFLAV.zip" ]; then mv ../$KFLAV.zip ../$KFLAV.zip.bak; fi
 
-zip -r9 ../$KFLAV.zip anykernel.sh META-INF ramdisk tools zImage; rm -f zImage
+zip -r9 ../$KFLAV.zip anykernel.sh META-INF ramdisk tools zImage; #rm -f zImage
