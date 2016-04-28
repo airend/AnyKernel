bd7ce9deb482062ac16941e80aa17ced69fc32fc
D. Andrei Măceș <dmaces@nd.edu>
2016-04-28 01:09:41 -0400

    bootzip.sh: Keep boot/system files around

--- bootzip.sh
+++ bootzip.sh
@@ -10,7 +10,8 @@ echo $ASSERT | sed -e "s/variant/${DEV}/g" > META-INF/com/google/android/updater
 
 cat updater-script >> META-INF/com/google/android/updater-script
 
-rm -rf boot.img system; mkdir -p system/lib/modules
+#rm -rf boot.img system;
+mkdir -p system/lib/modules
 
 cp $OUT/target/product/$DEV/boot.img .
 
@@ -18,4 +19,4 @@ cp $OUT/target/product/$DEV/system/lib/modules/* system/lib/modules
 
 if [ -f "../kernel-$DEV.zip" ]; then mv ../kernel-$DEV.zip ../kernel-$DEV.zip.bak; fi
 
-zip -r9 ../kernel-$DEV.zip boot.img system META-INF; rm -rf boot.img system
+zip -r9 ../kernel-$DEV.zip boot.img system META-INF; #rm -rf boot.img system
